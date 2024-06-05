a7112266100faacb53fadc7be6eb8e17ad2760ad
pwyll <diascamara38@gmail.com>
2024-06-05 11:10:22 -0300

    Fix

--- server/routes/restaurantRoutes.js
+++ server/routes/restaurantRoutes.js
@@ -4,10 +4,10 @@ import * as restaurantController from "../controller/restaurantController.js"
 
 const router = express.Router();
 
-router.get("/:name", restaurantController.getRestaurantByName)
+router.get("/:id", restaurantController.getRestaurantById)
 router.get("/", restaurantController.getAllRestaurants)
 router.post("/", restaurantController.createNewRestaurant)
-router.put("/:name", restaurantController.updateRestaurantByName)
-router.delete("/:name",restaurantController.deleteRestaurantByName)
+router.put("/:id", restaurantController.updateRestaurantById)
+router.delete("/:id",restaurantController.deleteRestaurantById)
 
 export default router
