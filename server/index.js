108ca9bc3739bc3970632c7a3eebc32497d6902e
pwyll <diascamara38@gmail.com>
2024-06-27 14:09:42 -0300

    host?

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import restaurantRouter from "./routes/restaurantRoutes.js"
+import "dotenv/config"
 
 const app = express();
 app.use(express.json())
@@ -8,7 +9,8 @@ app.use(cors())
 
 app.use("/restaurant", restaurantRouter)
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+//app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(process.env.PORT, () => console.log('Example app is listening on '+ process.env.PORT));
 
 
 export default app;
