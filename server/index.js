e936fc49a4fca99c76e9fb0a6d7015231b3baefe
pwyll <diascamara38@gmail.com>
2024-07-01 12:42:36 -0300

    pls

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from "express"
 import cors from "cors"
 import restaurantRouter from "./routes/restaurantRoutes.js"
-import "dotenv/config"
 
 const app = express();
 app.use(express.json())
