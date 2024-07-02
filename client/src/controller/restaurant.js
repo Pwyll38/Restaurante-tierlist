5e4e463f6b7849b2b1d4474434a23353698c3bd3
pwyll <diascamara38@gmail.com>
2024-07-02 12:33:24 -0300

    Yo?

--- client/src/controller/restaurant.js
+++ client/src/controller/restaurant.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 //DEV URL: `http://localhost:3000/restaurant/`
 //PROD URL: `https://restaurante-tierlist.onrender.com`
-const url = `https://restaurante-tierlist.onrender.com`
+const url = `https://restaurante-tierlist.onrender.com/restaurant/`
 
 function createRestaurant(restaurant) {
     return axios.post(`${url}`, restaurant)
@@ -22,7 +22,6 @@ async function deleteRestaurantById(id){
 }
 
 async function updateRestaurant(restaurant, id){
-    console.log(`${url}${id}`);
     await axios.put(`${url}${id}`, restaurant)
 }
 
